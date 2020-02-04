bdcd99ab2611202d5892b5c018522e2463bd2872
Sarah <sarahlohmes@gmail.com>
2020-02-03 17:13:52 -0800

    Display projects in table

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,30 +3,6 @@ import './projects.css';
 
 
 class Projects extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      displayNumber: 10,
-      moreToDisplay: false,
-    }
-
-    this.showMore = this.showMore.bind(this);
-
-    if (props.projects.length > this.state.displayNumber) {
-      this.state.projectsToDisplay = props.projects.slice(0, this.state.displayNumber);
-      this.state.moreToDisplay = true;
-    } else {
-      this.state.projectsToDisplay = props.projects;
-    }
-  }
-
-  showMore(e) {
-    this.setState({displayNumber: this.state.displayNumber + 10})
-  }
-
-
   render() {
     return (
       <div>
@@ -43,7 +19,7 @@ class Projects extends Component {
                   <th>Affordable Units</th>
                   <th>Total Units</th>
                 </tr>
-                {this.state.projectsToDisplay.map((project) => (
+                {this.props.projects.map((project) => (
                 <tr key={project.project_id}>
                   <td>{project.project_name}</td>
                   <td>{project.street_number}</td>
@@ -56,7 +32,6 @@ class Projects extends Component {
               ))}
               </tbody>
             </table>
-            { this.state.moreToDisplay ? <button onClick={ this.showMore }>more</button> : '' }
           </div>
       </div>
     )
